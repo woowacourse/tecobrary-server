3f2d97cfd5037e4c4f7317fc3161b172b2beadd0
backlo <hjun4512@naver.com>
2019-08-02 15:45:50 +0900

    feat: Spring Boot 초기 설정 변경

--- src/main/java/com/woowacourse/tecobrary/controller/TestController.java
+++ src/main/java/com/woowacourse/tecobrary/controller/TestController.java
@@ -1,15 +1,17 @@
 package com.woowacourse.tecobrary.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/test")
-@Slf4j
 public class TestController {
+
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/")
     public String test() {
