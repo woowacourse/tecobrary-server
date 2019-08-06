88d4e5d8e2c2b57d80a1af42d6e73c035a3f05ea
conatuseus <conatuseus@gmail.com>
2019-08-06 22:14:49 +0900

    feat: create Book domain
    Book 도메인 생성
    
    Squashed commit of the following:
    
    commit 40a5f9b7ccbb6e7cd1ba0d7d1da4da6b3223d8bf
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Tue Aug 6 22:14:24 2019 +0900
    
        create Book

---
+++ src/main/java/com/woowacourse/tecobrary/domain/Book.java
@@ -0,0 +1,40 @@
+package com.woowacourse.tecobrary.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@ToString
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, length = 11)
+    private Long id;
+
+    @Column(nullable = true, length = 255, unique = true)
+    private String url;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(nullable = false, length = 255)
+    private String author;
+
+    @Column(nullable = true, length = 255)
+    private String publisher;
+
+    @Column(nullable = false, length = 255)
+    private String isbn;
+
+    @Column(nullable = true, length = 255)
+    private String desc;
+
+    /* 추가해야 할 부분
+    private createdAt;
+    private updateAt;
+    private deleteAt;*/
+}
