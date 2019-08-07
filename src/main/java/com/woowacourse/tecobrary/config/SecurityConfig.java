3ff80db9eb443f5efda44cdfd96444b0265d9786
yk1028 <wongeunsong94@gmail.com>
2019-08-07 13:07:50 +0900

    feat: Spring Security 적용
    - github

---
+++ src/main/java/com/woowacourse/tecobrary/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.woowacourse.tecobrary.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableOAuth2Sso
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+}
