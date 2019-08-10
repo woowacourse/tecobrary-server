39ef99370f675abbb29b5b75e88ed87f9738306a
backlo <hjun4512@naver.com>
2019-08-10 19:46:43 +0900

    feat: WishService findByTitle 메소드 구현 및 TecobraryApplication 클래스 복구

--- src/main/java/com/woowacourse/tecobrary/service/WishService.java
+++ src/main/java/com/woowacourse/tecobrary/service/WishService.java
@@ -2,6 +2,7 @@ package com.woowacourse.tecobrary.service;
 
 import com.woowacourse.tecobrary.controller.dto.WishRequestDto;
 import com.woowacourse.tecobrary.domain.model.Wish;
+import com.woowacourse.tecobrary.exception.WishNotFoundException;
 import com.woowacourse.tecobrary.repository.WishRepository;
 import com.woowacourse.tecobrary.service.dto.WishResponseDto;
 import org.springframework.stereotype.Service;
@@ -27,4 +28,15 @@ public class WishService {
                 .title(saveWish.getTitle()).build();
     }
 
+    public WishResponseDto findByTitle(String title) {
+        Wish foundWish = wishRepository.findByTitle(title)
+                .orElseThrow(() -> new WishNotFoundException("요청한 책을 찾을 수 없습니다."));
+
+        return WishResponseDto.builder()
+                .id(foundWish.getId()).title(foundWish.getTitle())
+                .image(foundWish.getImage()).author(foundWish.getAuthor())
+                .publisher(foundWish.getPublisher()).isbn(foundWish.getIsbn())
+                .desc(foundWish.getDesc()).userId(foundWish.getUserId())
+                .build();
+    }
 }
