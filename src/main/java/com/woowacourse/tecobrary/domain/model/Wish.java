57d79ea57c688cad87454615f994c24480741f7f
backlo <hjun4512@naver.com>
2019-08-10 19:29:22 +0900

    feat: 희망 도서(Wish) Entity 생성 및 필드 구현

---
+++ src/main/java/com/woowacourse/tecobrary/domain/model/Wish.java
@@ -0,0 +1,49 @@
+package com.woowacourse.tecobrary.domain.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"image", "title", "author", "publisher", "isbn", "desc", "userId"})
+public class Wish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(name = "IMAGE")
+    private String image;
+
+    @Column(name = "TITLE", length = 100, unique = true)
+    private String title;
+
+    @Column(name = "AUTHOR", length = 20)
+    private String author;
+
+    @Column(name = "PUBLISHER", length = 50)
+    private String publisher;
+
+    @Column(name = "ISBN", length = 100)
+    private String isbn;
+
+    @Lob
+    @Column(name = "DESCRIPTION", columnDefinition = "TEXT")
+    private String desc;
+
+    @Column(name = "USER_ID")
+    private Long userId;
+
+    @Builder
+    public Wish(String image, String title, String author, String publisher, String isbn, String desc, Long userId) {
+        this.image = image;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.isbn = isbn;
+        this.desc = desc;
+        this.userId = userId;
+    }
+}
