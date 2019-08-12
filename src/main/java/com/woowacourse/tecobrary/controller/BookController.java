ab1bdc1bfd78483424b83c894ab088fdb3ba15ed
conatuseus <conatuseus@gmail.com>
2019-08-12 22:14:55 +0900

    refactor: controller 반환타입 ResponseEntity로 변경
    Squashed commit of the following:
    
    commit 296c44e326b4c0fc7ad1c7896490eb50926d9359
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Mon Aug 12 22:14:36 2019 +0900
    
        ResponseEntity 반환하도록 변경

--- src/main/java/com/woowacourse/tecobrary/controller/BookController.java
+++ src/main/java/com/woowacourse/tecobrary/controller/BookController.java
@@ -3,6 +3,8 @@ package com.woowacourse.tecobrary.controller;
 import com.woowacourse.tecobrary.service.BookService;
 import com.woowacourse.tecobrary.service.dto.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,27 +22,27 @@ public class BookController {
     }
 
     @GetMapping("/counts")
-    public BookNumbersResponseDto numberOfBooks() {
-        return bookService.numberOfBooks();
+    public ResponseEntity<BookNumbersResponseDto> numberOfBooks() {
+        return new ResponseEntity<>(bookService.numberOfBooks(), HttpStatus.OK);
     }
 
     @GetMapping
-    public BookInfoResponseDto findBookById(@RequestParam Long id) {
-        return bookService.findBookById(id);
+    public ResponseEntity<BookInfoResponseDto> findBookById(@RequestParam Long id) {
+        return new ResponseEntity<>(bookService.findBookById(id), HttpStatus.OK);
     }
 
     @PostMapping
-    public BookCreateResponseDto createBook(@RequestBody BookCreateRequestDto bookCreateRequestDto) {
-        return bookService.createBook(bookCreateRequestDto);
+    public ResponseEntity<BookCreateResponseDto> createBook(@RequestBody BookCreateRequestDto bookCreateRequestDto) {
+        return new ResponseEntity<>(bookService.createBook(bookCreateRequestDto), HttpStatus.OK);
     }
 
     @DeleteMapping
-    public BookDeleteResponseDto deleteBook(@RequestParam Long id) {
-        return bookService.deleteBook(id);
+    public ResponseEntity<BookDeleteResponseDto> deleteBook(@RequestParam Long id) {
+        return new ResponseEntity<>(bookService.deleteBook(id), HttpStatus.OK);
     }
 
     @GetMapping("/{keyword}")
-    public List<BookInfoResponseDto> findBookByKeyword(@PathVariable String keyword) {
-        return bookService.findBooksByKeyword(keyword);
+    public ResponseEntity<List<BookInfoResponseDto>> findBookByKeyword(@PathVariable String keyword) {
+        return new ResponseEntity<>(bookService.findBooksByKeyword(keyword), HttpStatus.OK);
     }
 }
